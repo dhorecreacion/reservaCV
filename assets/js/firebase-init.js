d1d64dfdbdbf4f2ea9232ea3c8659380ee1212b0
dhorecreacion <dhorecreacion@gmail.com>
2026-05-07 16:26:17 -0500

    v1

---
+++ assets/js/firebase-init.js
@@ -0,0 +1,8 @@
+firebase.initializeApp({
+  apiKey: "AIzaSyBSG7fAe_lIaiIaocyo9aaGgW7ZdwH7Q-k",
+  authDomain: "citascv-3a82a.firebaseapp.com",
+  projectId: "citascv-3a82a",
+  storageBucket: "citascv-3a82a.firebasestorage.app",
+  messagingSenderId: "839739344888",
+  appId: "1:839739344888:web:efcedb7ef312a53f53780d"
+});
